8eb13be7a33d24a3f743798c3baa31c0e9ed216f
Veaceslav Mindru <mindruv@gmail.com>
2021-01-11 21:06:24 +0100

    widget, and working timers

--- pit.py
+++ pit.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 from kivy.app import App
+from kivy.utils import get_color_from_hex
 from kivy.lang import Builder
 from kivy.uix.label import Label
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import NumericProperty
 from kivy.animation import Animation
 
@@ -11,34 +12,53 @@ Builder.load_string('''
 
 <RootWidget>:
     #:import randint  random.randint
+    #:import get_color_from_hex kivy.utils.get_color_from_hex
     orientation: "vertical"
-    SecondLabel:
-        id: circle
-        canvas:
-            Color:
-                rgb: 1,0,0
-            Line:
-                circle:self.center_x, self.center_y, 60
-                width: 60
-    CountDownLbl:
-        timer_duration: 3
-        id: anim_label
-        font_size: 30
-        text: "{}.000".format(self.timer_duration)
-        canvas:
-            Color:
-                rgb: 0,1,0
-            Line:
-                circle:self.center_x, self.center_y, 90, 0, self.angle % 360
-                width: 30
-    Button:
-        size_hint_y: 0.1
-        text: "Start"
-        on_press: anim_label.start()
+    BoxLayout:
+        orientation: "vertical"
+        CountDownLbl:
+            bg_color: 1,0,0
+            timer_duration: 1
+            markup: True
+            id: anim_label
+            font_size: 30
+            text: "[color={}]Kart1[/color] \\n [color={}]{}.000[/color]".format(self.Green, self.Red, self.timer_duration)
+            canvas:
+                Color:
+#                    rgb: get_color_from_hex(self.Green) if self.in_progress else get_color_from_hex(self.Red)
+                    rgb: get_color_from_hex(self.Yellow)
+                Line:
+                    circle:self.center_x, self.center_y, 90
+                    width: 15
+        CountDownLbl:
+            bg_color: 1,0,0
+            timer_duration: 1
+            markup: True
+            id: anim_label2
+            font_size: 30
+            text: "[color={}]Kart1[/color] \\n [color={}]{}.000[/color]".format(self.Green, self.Red, self.timer_duration)
+            canvas:
+                Color:
+#                    rgb: get_color_from_hex(self.Yellow) if self.in_progress else get_color_from_hex(self.Yellow)
+                    rgb: get_color_from_hex(self.Yellow)
+                Line:
+                    circle:self.center_x, self.center_y, 90
+                    width: 15
+    BoxLayout:
+        o1rientation: "horizontal"
+        Button:
+            size_hint_y: 0.1
+            text: "Start"
+            on_press: anim_label.start()
+
+        Button:
+            size_hint_y: 0.1
+            text: "Start"
+            on_press: anim_label2.start()
         ''')
 
 
-class RootWidget(FloatLayout):
+class RootWidget(BoxLayout):
     pass
 
 
@@ -47,6 +67,10 @@ class SecondLabel(Label):
 
 
 class CountDownLbl(Label):
+    Yellow = "#F9F900"
+    Red = "F90000"
+    Green = "41FD00"
+    bg_color = get_color_from_hex(Red)
     angle = NumericProperty(0)
     timer_duration = NumericProperty(0)
 
@@ -64,12 +88,12 @@ class CountDownLbl(Label):
             self.anim.start(self)
 
     def finish(self, animation, widget):
-        widget.text = "FINISHED"
+        widget.text = "[color={}]Kart1[/color] \n [color={}] GO!!!! [/color]".format(self.Green, self.Green)
         self.in_progress = False
 
     def update_timer(self, animation, widget, progression):
         text = ((self.timer_duration * 60000)*(1-progression))/60000
-        widget.text = "{0:.3f}".format(float(text))
+        widget.text = "[color={2}]Kart1[/color] \n [color={1}]{0:.3f}[/color]".format(float(text), self.Red, self.Green)
 
 
 class TestApp(App):
